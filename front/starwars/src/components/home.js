091012bb8317be1dac2924a55ebf4a2fc1752ed8
Zhassulan1 <zhkainazarov@gmail.com>
2024-06-01 00:39:27 +0500

    adding crawl text

--- front/starwars/src/components/home.js
+++ front/starwars/src/components/home.js
@@ -14,7 +14,26 @@ class Home extends React.Component {
         return (
             <>
                 <Header />
-                This is site about Star Wars. Here you can find information about planets in Star Wars universe.
+                
+
+                <div className="fade"></div>
+
+                <section className="star-wars">
+
+                <div className="crawl">
+
+                    <div className="title">
+                        <p> Here you can find information about Star Wars universe</p> <br />
+                        <h1>Star Wars</h1>
+                    </div>
+                    
+                        <p>It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.</p>      
+                        <p>During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.</p>
+                        <p>Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy…</p>
+
+                    </div>
+
+                </section>
             </>
         )
     }
