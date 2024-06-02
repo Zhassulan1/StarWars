40c1b04eaec6769b4412fec07482c97c1b0e79d2
Zhassulan1 <zhkainazarov@gmail.com>
2024-06-02 16:11:14 +0500

    added some error handling so that backend does not exit with error

--- back/index.js
+++ back/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors())
 const BASE_URL = "https://swapi.dev/api/";
 
+
 app.get('/', (request, response) => {
     const ipAddress = request.ip;
     response.send("Hello from the back end server!");
@@ -17,22 +18,46 @@ app.get('/', (request, response) => {
 
 app.get('/api/:item/page/:page', (request, response) => {
     const ipAddress = request.ip;
-    axios.get(`${BASE_URL}${request.params.item}?page=${request.params.page}`).then((res) => {
-        response.send(res.data);
-        console.log(`GET api/${request.params.item}/page/${request.params.page}  \nfrom ip: ${ipAddress}\n`);
-        response.end();
-    })
-})
+    const item = request.params.item;
+    const page = request.params.page;
+    console.log(`GET api/${item}/page/${page}  \nfrom ip: ${ipAddress}`);
 
+    let error = null
+    axios.get(`${BASE_URL}${item}?page=${page}`).catch(err => {
+        error = err;
+    }).then((res) => {
+        if (error) {
+            response.sendStatus(404);
+            console.log(`ERROR: Axios error\n`);
+            return;
+        }
+        response.send(res.data);
+        response.end();
+        console.log(`\n`);
+    })
+    
+})
 
 
 // Details
 app.get('/api/:item/:itemId', (request, response) => {
     const ipAddress = request.ip;
-    axios.get(`${BASE_URL}${request.params.item}/${request.params.itemId}`).then((res) => {
+    const item = request.params.item;
+    const itemId = request.params.itemId;
+    console.log(`GET api/${item}/${itemId}  \nfrom ip: ${ipAddress}`);
+
+    let error = null
+    axios.get(`${BASE_URL}${item}/${itemId}`).catch(err => {
+        error = err;
+    }).then((res) => {
+        if (error) {
+            response.sendStatus(404);
+            console.log(`ERROR: Axios error\n`);
+            return;
+        }
         response.send(res.data);
-        console.log(`GET api/${request.params.item}/${request.params.itemId}  \nfrom ip: ${ipAddress}\n`);
         response.end();
+        console.log(`\n`);
     })
 })
 
@@ -40,14 +65,35 @@ app.get('/api/:item/:itemId', (request, response) => {
 // Search
 app.get('/api/:item/search/:query', (request, response) => {
     const ipAddress = request.ip;
-    axios.get(`${BASE_URL}${request.params.item}/?search=${request.params.query}`).then((res) => {
+    const item = request.params.item;
+    const query = request.params.query;
+    console.log(`GET api/${item}/search/${query}  \nfrom ip: ${ipAddress}`);
+
+    let error = null
+    axios.get(`${BASE_URL}${item}/?search=${query}`).catch(err => {
+        error = err;
+    }).then((res) => {
+        if (error) {
+            response.sendStatus(404);
+            console.log(`ERROR: Axios error\n`);
+            return;
+        }
         response.send(res.data);
-        console.log(`GET api/${request.params.item}/search/${request.params.query}  \nfrom ip: ${ipAddress}\n`);
         response.end();
+        console.log(`\n`);
     })
 })
 
 
+app.use((request, response) => {
+    const requestURL = request.get('host') + request.originalUrl;
+    const ipAddress = request.ip;
+    const method = request.method;
+    
+    console.log(`NOT FOUND\n${method} ${requestURL}   \nfrom ip: ${ipAddress}\n`);
+    response.sendStatus(404);
+    response.end();
+});
 
 
 const PORT = 3001;
